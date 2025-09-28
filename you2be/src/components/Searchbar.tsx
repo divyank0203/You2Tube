583f5b7acd05946b36436afd5658aa0286e5ead8
divyank0203 <divyankh444@gmail.com>
2025-09-28 20:53:51 +0530

    searchbar update

--- you2be/src/components/Searchbar.tsx
+++ you2be/src/components/Searchbar.tsx
@@ -7,8 +7,8 @@ function Searchbar() {
  
      
     <div className="relative">
-        <div className="w-120 flex focus:ring-1 focus:ring-blue-300 text-gray-900 border border-gray-500 rounded-full mt-1.5 p-1 pl-3 text-sm pr-">
-            <input type="search" id="default-search" className=" focus:border-blue-500 w-full outline-none p-2 ps-10 text-sm text-gray-900 border border-none rounded-full bg-slate-950   dark:border-none dark:placeholder-gray-400 dark:text-white " placeholder="Search" required />
+        <div className="w-120 flex focus:border-2  focus:border-blue-500 focus:ring-1 focus:ring-blue-300 bg-slate-950 text-gray-900 border border-gray-500 rounded-full mt-1.5 p-1 pl-3 text-sm">
+            <input type="search" id="default-search" className=" w-full outline-none p-2 ps-10 text-sm text-gray-900 border border-none rounded-full bg-slate-950   dark:border-none dark:placeholder-gray-400 dark:text-white " placeholder="Search" required />
              <button type="submit" className="text-gray-400 font-bold bg-slate-950 absolute end-2.5 bottom-2.5 hover:cursor-pointer  hover:bg-gray-800 focus:ring-1 focus:outline-none focus:ring-blue-300 rounded-full inline-flex items-center text-sm px-4 py-1.5">
                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
