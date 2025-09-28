3fbd66e17e4822583ab6c60ee7ab32239902888a
divyank0203 <divyankh444@gmail.com>
2025-09-28 13:44:15 +0530

    more button - videocard component

--- you2be/src/components/VideoCard.tsx
+++ you2be/src/components/VideoCard.tsx
@@ -12,20 +12,24 @@ function VideoCard(props: any) {
                 <div className='col-span-1'>
                     <img className='rounded-full w-10 h-10' src={props.avatar} alt="" />
                 </div>
-                <div className='col-span-11 pl-3'>
+                <div className='col-span-10 pl-3'>
                     <div className=' font-bold text-base'>
                         {props.title}
                     </div>
                     
-                    <div className='col-span-11 text-gray-600 text-sm'>
+                    <div className='col-span-10 text-gray-600 text-sm'>
                         {props.channel}
                         <br />
                         {props.views} Views | {props.time} Ago
                     </div>
                     
                 </div>
+                <div className='col-span-1'>
+                    <img className='hover:cursor-pointer hover:bg-gray-500/40 hover:rounded-full h-8 w-8
+                    p-1.5' 
+                    src="/more (1).png" alt="" />
+                </div>
 
-                
             </div>
         </div>
     </div>
