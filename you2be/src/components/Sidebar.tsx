d4fc0aec107baa6769fca0113cfadc2f8bed28da
divyank0203 <divyankh444@gmail.com>
2025-10-03 19:00:23 +0530

    sidebar

---
+++ you2be/src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='grid grid-rows-12'>
+        <div>Home</div>
+        <div>Shorts</div>
+        <div>Subscriptions</div>
+        <div>History</div>
+        <div>Playlists</div>
+        <div>Your Videos</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Sidebar
